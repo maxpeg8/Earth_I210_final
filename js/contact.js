17a343d11e078f8d969ba2f3e0cef0ef53e5bd3b
Max Pegram <maxpeg850@gmail.com>
2025-05-05 21:05:17 -0400

    adding a dom and crud

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+import { app } from "./app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+console.log("hi");
